257bd3c3d8d572668fed7d53a046c15daf3a4933
Skyler Blakeslee <skylerblakeslee@gmail.com>
2021-02-06 14:53:21 -0800

    updated all search queries for ivaee

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,30 @@ console.log("hi")
 var genreInput = $("#genreSelect");
 var decadeInput = $("#decadeSelect");
 var ratingInput = $("#ratingSelect");
-var actorInput = $("#actorSelect");
+var actorInput = $("#name");
 var search = $("#srchBtn");
 var clear = $("#clear");
 
 // genres manually selected from imdb genre list
 var genres = ["Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "Film Noir", "History", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Sport", "Thriller", "War", "Western"]
-var decades = ["1950s", "1960s", "1970s", "1980s", "1990s", "2000s", "2010s", "2020s"]
-var ratings = ["8-10", "6-8", "4-6", "2-4", "0-2"]
+var decades = [
+    { display: "1950s", query: "YearRange_Start=1950&YearRange_End=1959" },
+    { display: "1960s", query: "YearRange_Start=1960&YearRange_End=1969" },
+    { display: "1970s", query: "YearRange_Start=1970&YearRange_End=1979" },
+    { display: "1980s", query: "YearRange_Start=1980&YearRange_End=1989" },
+    { display: "2010s", query: "YearRange_Start=2010&YearRange_End=2019" },
+    { display: "1990s", query: "YearRange_Start=1990&YearRange_End=1999" },
+    { display: "2000s", query: "YearRange_Start=2000&YearRange_End=2009" },
+    { display: "2020s", query: "YearRange_Start=2020&YearRange_End=2019" }
+]
+
+var ratings = [
+    { display: "8+", query: "Minimum_IvaRating=80" },
+    { display: "6+", query: "Minimum_IvaRating=60" },
+    { display: "4+", query: "Minimum_IvaRating=40" },
+    { display: "2+", query: "Minimum_IvaRating=20" },
+    { display: "0+", query: "Minimum_IvaRating=0" }
+];
 
 for (var i = 0; i < genres.length; i++) {
     console.log("for loop")
@@ -27,23 +43,56 @@ for (var i = 0; i < genres.length; i++) {
 
 for (var i = 0; i < decades.length; i++) {
     console.log("for loop")
-    var option = $("<option>").attr("value", decades[i]).text(decades[i])
+    var option = $("<option>").attr("value", decades[i].query).text(decades[i].display)
     decadeInput.append(option)
     console.log(option)
 }
 
 for (var i = 0; i < ratings.length; i++) {
     console.log("for loop")
-    var option = $("<option>").attr("value", ratings[i]).text(ratings[i])
+    var option = $("<option>").attr("value", ratings[i].query).text(ratings[i].display)
     ratingInput.append(option)
     console.log(option)
 }
 
 $("#srchBtn").on("click", function () {
-    console.log($("#genreSelect").val())
-    var genre = $("#genreSelect").val()
-    var genreQuery = "Genres=" + genre
-    fetch("https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/search/?" + genreQuery, {
+    if ($("#genreSelect").val()) {
+        var genre = $("#genreSelect").val()
+        console.log(genre)
+        var genreQuery = "Genres=" + genre
+    }
+    else {
+        var genreQuery = ""
+    };
+
+    if ($("#decadeSelect").val()) {
+        var decade = $("#decadeSelect").val()
+        console.log(decade)
+        var decadeQuery = "&" + decade
+    }
+    else {
+        var decadeQuery = ""
+    };
+
+    if ($("#ratingSelect").val()) {
+        var rating = $("#ratingSelect").val()
+        console.log(rating)
+        var ratingQuery = "&" + rating
+    }
+    else {
+        var ratingQuery = ""
+    };
+
+    if ($("#name").val()) {
+        var actorName = $("#name").val()
+        console.log(actorName)
+        var actorQuery = "&PersonNames=" + actorName
+    }
+    else {
+        var actorName = ""
+    };
+
+    fetch("https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/search/?" + genreQuery + actorQuery + "&ReleaseCountries=US" + decadeQuery + ratingQuery + "&ProgramTypes=Movie", {
         "method": "GET",
         "headers": {
             "content-type": "application/json",
@@ -91,13 +140,3 @@ fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?
         }
         console.log(movieMatch);
     });
-
-    // Example ivaee querry
-    // fetch("https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/search/?Genres=Fantasy&YearRange_Start=2010&YearRange_End=2020", {
-    //     "method": "GET",
-    //     "headers": {
-    //         "content-type": "application/json",
-    //         "x-rapidapi-key": "cad95bd25cmsh6e5669cc54c4eb1p11eeb2jsn2eebb15c9353",
-    //         "x-rapidapi-host": "ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com"
-    //     }
-
